6d19a5544f3b873bde1b0b2332fb600be6bbbb70
David Jefts <jeftsd@my.erau.edu>
2021-04-03 22:49:14 -0400

    Scrapes HTML span tags for text

--- src/fb_scraper_api.py
+++ src/fb_scraper_api.py
@@ -1,9 +1,4 @@
 """
-Running Experiment on Facebook Account John Keck
-
-Username: TimElvResearch@gmail.com
-Password: keckW2323#
-
 This file contains all the methods and structures necessary to scrape information from an input Facebook page
 """
 
@@ -17,22 +12,47 @@ import wget
 from time import sleep
 
 
-def get_methods(object, spacing = 40):
-    methodList = []
-    for method_name in dir(object):
+def get_name(mystery_object):
+    print(type(mystery_object).__name__)
+
+
+def get_methods(mystery_object, spacing = 40):
+    """
+    Prints the details for all the methods associated with the Class of the input object
+    :param mystery_object: input object
+    :param spacing: Space between the method and its description
+    :return: None
+    """
+    print(type(mystery_object).__name__)
+    method_list = []
+    for method_name in dir(mystery_object):
         try:
-            if callable(getattr(object, method_name)):
-                methodList.append(str(method_name))
+            if callable(getattr(mystery_object, method_name)):
+                method_list.append(str(method_name))
         except:
-            methodList.append(str(method_name))
-    processFunc = (lambda s: ' '.join(s.split())) or (lambda s: s)
-    for method in methodList:
+            method_list.append(str(method_name))
+    process_func = (lambda s: ' '.join(s.split())) or (lambda s: s)
+    for method in method_list:
         try:
-            print(str(method.ljust(spacing)) + ' ' +
-                  processFunc(str(getattr(object, method).__doc__)))
+            print(str(method.ljust(spacing)) + ' ' + process_func(str(getattr(mystery_object, method).__doc__)))
         except:
-            print(method.ljust(spacing) + '' + ' getattr() failed')
-    print('\n\n')
+            print(method.ljust(spacing) + '\t' + ' getattr() failed')
+
+
+def get_variables(mystery_object):
+    """
+    Prints out all the attributes associated with the Class of the input object
+    :param mystery_object: input object
+    :return: None
+    """
+    variable_list = []
+    for variable_name in dir(mystery_object):
+        try:
+            if not callable(getattr(mystery_object, variable_name)):
+                variable_list.append(str(variable_name))
+        except:
+            variable_list.append(str(variable_name))
+    print(*variable_list, sep = ", ")
 
 
 def get_user_information(driver):
@@ -41,14 +61,16 @@ def get_user_information(driver):
     hobbies = []
     friends = []
     information = driver.find_elements_by_tag_name('span')
-    get_methods(information[0])
+    sleep(1)
     return information
 
 
-def get_driver(email = 'TimElvResearch@gmail.com', password = 'keckW2323#',
+def get_driver(email = 'TimElvResearch@gmail.com', password = 'keckW2323#', username = 'John Keck',
                permalink = 'https://www.facebook.com/john.keck.125'):
     """
     Starts up the Firefox driver and navigates to the input user's Facebook Homepage
+    Default parameters are the email/password/website for the Semantic Security test user "John Keck"
+    :param username: The name of the user
     :param email: Facebook login email
     :param password: Facebook login password
     :param permalink: https://www.facebook.com/[facebook username]
@@ -63,23 +85,51 @@ def get_driver(email = 'TimElvResearch@gmail.com', password = 'keckW2323#',
     driver = webdriver.Firefox(options = options,
                                executable_path = rel_dir + '/Firefox_Drivers/geckodriver.exe',
                                service_log_path = rel_dir + '/Firefox_Drivers/geckodriver.log')
-    print("Firefox driver load success!")
+    wait = WebDriverWait(driver, 10)
+    driver.implicitly_wait(10)
+    print("Firefox driver load success!\n\n")
     
+    # Log in to Facebook
     driver.get('https://www.facebook.com/')
+    email_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
+    password_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
     
-    username_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
-    password_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
-    
-    username_input.clear()
-    username_input.send_keys(str(email))
+    email_input.clear()
+    email_input.send_keys(str(email))
     password_input.clear()
     password_input.send_keys(str(password))
     
-    button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[name='login']")))
+    button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[name='login']")))
     button.send_keys(Keys.RETURN)
     
-    sleep(10)
-    
+    wait.until_not(EC.title_contains("Log In or Sign Up"))
     driver.get(permalink)
+    wait.until(EC.title_contains("{} | Facebook".format(username)))
     
-    return driver
+    return driver, wait
+
+
+def get_element_children(element):
+    element.find_elements_by_xpath(".//*")
+
+
+def scroll_down(driver):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
+def scroll_down_v2(driver, scroll_pause_time = 0.5):
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        
+        # Wait to load page
+        sleep(scroll_pause_time)
+        
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
--- src/main.py
+++ src/main.py
@@ -7,8 +7,9 @@ Password: keckW2323#
 from fb_scraper_api import *
 from time import sleep
 
-user_permalink = "https://www.facebook.com/john.keck.125"
-user_page = 'john.keck.125'
+john_permalink = "https://www.facebook.com/john.keck.125"
+john_username = 'john.keck.125'
+john_name = "John Keck "
 
 
 class User:
@@ -20,10 +21,23 @@ class User:
 
 
 if __name__ == '__main__':
-    firefox = get_driver()
+    firefox, wait = get_driver()
+    scroll_down(firefox)
     user_info = get_user_information(firefox)
-    print(user_info)
+    get_methods(user_info[0])
+    get_variables(user_info[0])
+    print('\n\n')
     
-    sleep(3)
+    # print(user_info)
+    for span_tag in user_info:
+        if span_tag.text is not None and span_tag.text != '':
+            print("Span:", "{} - '{}'".format(span_tag, span_tag.text))
+            children = get_element_children(span_tag)
+            try:
+                for child in children:
+                    print("\t{}".format(child.text))
+            except TypeError:
+                print("\t<No children>")
     
+    sleep(5)
     firefox.quit()
